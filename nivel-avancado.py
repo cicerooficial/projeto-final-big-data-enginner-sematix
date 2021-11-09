fc4eeff3343397923ca94c75aadfcd11a3569b8a
cicerooficial <cicerooficial@gmail.com>
2021-11-09 09:54:33 -0300

    Update nivel-avancado.py

--- nivel-avancado.py
+++ nivel-avancado.py
@@ -1,1 +1,1 @@
-#### Replicar as visualizações do site “https://covid.saude.gov.br/”, porém acessando diretamente a API de Elastic
+#### Replicar as visualizações do site https://covid.saude.gov.br/, porém acessando diretamente a API de Elastic
