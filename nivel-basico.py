2f4f2f6c13d7889bf4d23a97a383c77844d9d286
cicerooficial <cicerooficial@gmail.com>
2021-11-09 10:18:15 -0300

    Update nivel-basico.py

--- nivel-basico.py
+++ nivel-basico.py
@@ -6,34 +6,39 @@
 curl -O https://mobileapps.saude.gov.br/esus-vepi/files/unAFkcaNDeXajurGB7LChj8SgQYS2ptm/04bd3419b22b9cc5c6efac2c6528100d_HIST_PAINEL_COVIDBR_06jul2021.rar
 ```
 
-Instale o unrar para descompactar o arquivo .rar 
+# Instale o unrar para descompactar o arquivo .rar 
 
 ```shell
 sudo apt install unrar
+```
+```shell
 unrar x 04bd3419b22b9cc5c6efac2c6528100d_HIST_PAINEL_COVIDBR_06jul2021.rar
-
+```
 #Enviar os arquivos .csv para a pasta input
+```shell
 sudo mv *.csv /home/cicero/projeto-final-spark/spark/input
 ```
 
-Inicie todos os serviços:
+# Inicie todos os serviços:
 
 ```shell
 docker-compose -f docker-compose-parcial.yml up -d
 ```
 
-Envie os arquivos para o DFS
-
+# Envie os arquivos para o DFS
 ```shell
 #Entre no namenode
 docker exec -it namenode bash
-
+```
+```shell
 #Crie a pasta projeto-final-spark no HDFS para salvar o arquivo de dados .rar
 hdfs dfs -mkdir -p /user/cicero/projeto-final-spark
-
+```
+```shell
 #Envie o arquivo de dados .rar para a pasta projeto-final-spark no HDFS
 hdfs dfs -put /input/*.csv /user/cicero/projeto-final-spark
-
+```
+```shell
 #Confirme se o arquivo foi enviado
 hdfs dfs -ls /user/cicero/projeto-final-spark
 ```
