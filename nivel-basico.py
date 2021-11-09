cb8227da453d62f97328562b7b188a1557766f6c
cicerooficial <cicerooficial@gmail.com>
2021-11-09 10:15:19 -0300

    Update nivel-basico.py

--- nivel-basico.py
+++ nivel-basico.py
@@ -1,6 +1,6 @@
-#### Enviar os dados para o HDFS
+# Enviar os dados para o HDFS
 
-No WSL2, baixe o arquivo de dados .rar dentro da pasta spark:
+# No WSL2, baixe o arquivo de dados .rar dentro da pasta spark:
 
 ```shell
 curl -O https://mobileapps.saude.gov.br/esus-vepi/files/unAFkcaNDeXajurGB7LChj8SgQYS2ptm/04bd3419b22b9cc5c6efac2c6528100d_HIST_PAINEL_COVIDBR_06jul2021.rar
@@ -38,33 +38,47 @@ hdfs dfs -put /input/*.csv /user/cicero/projeto-final-spark
 hdfs dfs -ls /user/cicero/projeto-final-spark
 ```
 
+------
+
 #### Otimizar todos os dados do hdfs para uma tabela Hive particionada por município
 
-Acesse o jupyter-notebook
+#Acesse o jupyter-notebook
 
 ```shell
 http://localhost:8889/
 ```
 
-Abra um arquivo PySpark chamado projeto-final-spark
+#Abra um arquivo PySpark chamado projeto-final-spark
+
+# Acesse os arquivos .CSV pelo HDFS
+!hdfs dfs -ls "hdfs:///user/cicero/projeto-final-spark/"
 
 
 
-#### Criar as 3 visualizações pelo Spark com os dados enviados para o HDFS 
 
 
 
-#### Salvar a primeira visualização como tabela Hive
+------
+
+# Criar as 3 visualizações pelo Spark com os dados enviados para o HDFS 
 
 
+------
 
-#### Salvar a segunda visualização com formato parquet e compressão snappy
-
-
-
-#### Salvar a terceira visualização em um tópico no Kafka
+# Salvar a primeira visualização como tabela Hive
 
 
+------
 
-#### Criar a visualização pelo Spark com os dados enviados para o HDFS
+# Salvar a segunda visualização com formato parquet e compressão snappy
+
+
+------
+
+# Salvar a terceira visualização em um tópico no Kafka
+
+
+------
+
+# Criar a visualização pelo Spark com os dados enviados para o HDFS
 
